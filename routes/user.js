79830bfc024b314e1c692fc747171a7ef62a9d99
omid saghatchian <omidsaghatchian@gmail.com>
2020-07-20 21:38:54 +0430

    add signup route

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const UserController = require("../controllers/userController");
+const validation = require("../tools/validation");
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+
+
+/*********************************************************************************
+* add new Blogger
+**********************************************************************************/
+router.post('/signup', validation.registerForm, UserController.signUpBlogger);
+
 
 module.exports = router;
