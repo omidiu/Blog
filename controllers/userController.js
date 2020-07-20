b78e8e1fc085d8406687fb4554f453ff05add1e8
omid saghatchian <omidsaghatchian@gmail.com>
2020-07-20 21:37:15 +0430

    implement signup

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,50 @@
 const User = require("../models/user");
-
-
+const bcrypt = require('bcrypt');
+const saltRounds = 8;
 
 /*********************************************************************************
 * add new Blogger (POST)
 **********************************************************************************/
-exports.signUpBlogger = (req, res) => {
-  res.send('Not implemented yet: add blogger after validation')
+exports.signUpBlogger = async(req, res) => {
+  try {
+    // get information from request body.
+    let {firstName, lastName, username, password, password_confirmation, sex, mobile} = req.body;
+
+    // check username exist
+    const userExist = await User.findOne({username: username});
+    if (userExist) return res.status(400).send('This username is already exist');
+
+
+    // check mobile exist
+    const mobileExist = await User.findOne({mobile: mobile});
+    if (mobileExist) return res.status(400).send('This number is already exist');
+
+    // hash password
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    
+
+    // create user
+    const user = new User({
+      firstName: firstName,
+      lastName: lastName, 
+      username: username, 
+      password: password, 
+      password_confirmation: password_confirmation, 
+      sex: sex, 
+      mobile: mobile
+    });
+
+    // save user
+    const savedUser = await user.save(user);
+    res.status(201).send("created successfully");
+
+
+
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(); // internal server error
+  }
+  
 }
 
 
