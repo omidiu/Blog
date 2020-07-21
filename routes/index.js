e935566f9cf235beea7e6ff4f804cce2f7d8e23a
omid saghatchian <omidsaghatchian@gmail.com>
2020-07-21 21:22:30 +0430

    Add routes "/", "/signup", "/login"

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const indexController = require("../controllers/indexController");
+const validation = require('../tools/validation');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+
+
+
+/*********************************************************************************
+* Display pages/articles/index.ejs if authentication is successfully (GET)
+********************************************************************* *************/
+router.get('/', validation.authenticateToken, indexController.mainArticlePage);
+
+
+
+/*********************************************************************************
+* Display signup page (GET)
+**********************************************************************************/
+router.get('/signup', validation.redirectToMainPageWithValidtoken, indexController.signupPage);
+
+
+
+/*********************************************************************************
+* Display login page (GET)
+**********************************************************************************/
+router.get('/login', validation.redirectToMainPageWithValidtoken, indexController.loginPage);
+
+
+
+
+
 
 module.exports = router;
