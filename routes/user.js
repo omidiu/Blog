3c244b27198c044753afeff88d171b7fcff85927
omid saghatchian <omidsaghatchian@gmail.com>
2020-07-22 08:24:16 +0430

    Add "/:username" route

--- routes/user.js
+++ routes/user.js
@@ -3,21 +3,11 @@ const router = express.Router();
 const UserController = require("../controllers/userController");
 const validation = require("../tools/validation");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
-
 
 /*********************************************************************************
 * add new Blogger
 **********************************************************************************/
 router.post('/signup', validation.registerForm, UserController.signUpBlogger);
-
-
-
-
 
 
 /*********************************************************************************
@@ -26,6 +16,11 @@ router.post('/signup', validation.registerForm, UserController.signUpBlogger);
 router.post('/login', UserController.loginBlogger);
 
 
+
+/*********************************************************************************
+* Display Blogger page 
+**********************************************************************************/
+router.get("/:username", validation.authenticateToken, UserController.bloggerDetailsPage);
 
 
 module.exports = router;
