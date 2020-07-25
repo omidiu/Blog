e87b076c5798d8aa872022f9a4438c404b990a2b
omid saghatchian <omidsaghatchian@gmail.com>
2020-07-25 20:48:57 +0430

    add "changePassForm" and "oldPasswordCorrect" validation

--- tools/validation.js
+++ tools/validation.js
@@ -1,5 +1,9 @@
 const Joi = require("@hapi/joi");
 const jwt = require("jsonwebtoken");
+const User = require('../models/user');
+const bcrypt = require('bcrypt');
+
+
 
 /*********************************************************************************
 * Validate register form
@@ -218,6 +222,106 @@ exports.editProfileForm = (req, res, next) => {
   
 }
 
+
+
+
+
+/*********************************************************************************
+* Validate password is correct (PUT)
+**********************************************************************************/
+exports.oldPasswordCorrect = async (req, res, next) => {
+
+  // get old pass
+  let {oldPass} = req.body;
+
+  // check password is not empty
+  if (!oldPass) return res.status(400).send({
+    message: "Your previous password shouldn't be empty", 
+    messageClass: "alert-danger"
+  });
+
+  // get username
+  let username = req.user.username;
+
+
+
+  // get user
+  let user = await User.findOne({username: username});
+
+  // check oldpass is correct or not
+  let passwordCorrect = await bcrypt.compare(oldPass, user.password);
+
+  // not correct -> re-render "pages/users/edit" with proper error message.
+  if (!passwordCorrect) return res.status(400).render('pages/users/edit', {
+    message: "Your password is not correct", 
+    messageClass: "alert-danger"
+  });
+
+  
+  // old pass is correct 
+  next();
+  
+
+};  
+
+/*********************************************************************************
+* Validate change password form (PUT)
+**********************************************************************************/
+exports.changePassForm = (req, res, next) => {
+
+  // get info from req.body
+  let {newPass, newPass_confirmation} = req.body
+
+  // define {newpass, newPass_confirmation }
+  let newPassWithConfirmation = {
+    newPass, 
+    newPass_confirmation
+  }
+
+  // Atleast 1 letter, 1 number, 1 special character 
+  // min 8 characters, max 20 characters 
+  let passwordPattern = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*()_+])[A-Za-z\d!@#$%^&*()_+]{8,20}/; 
+
+
+  // Define newPassword form schema with specific error for each case. 
+  let schema = Joi.object().keys({
+                
+      newPass: 
+                Joi
+                .string()
+                .min(8)
+                .max(30)
+                .pattern(passwordPattern)
+                .required()
+                .messages({
+                  "string.min": "Your password should contain at least 8 character",
+                  "string.max": "Your password should contain at last 30 character",
+                  "string.pattern.base": "Your password should contain special character, at least a letter, at least a number"
+                }),
+  
+      newPass_confirmation:  
+                Joi
+                .any()
+                .valid(Joi.ref('newPass'))
+                .required()
+                .messages({
+                  'any.only': "Please confirm Your password correctly"
+                }),
+              
+  });
+
+  // check data is valid or not
+  let {error} = schema.validate(newPassWithConfirmation);
+
+  // if any error render "pages/signup" with proper error message
+  if (error) return res.status(400).render('pages/users/edit', {
+    message: error.details[0].message,
+    messageClass: "alert-danger"
+  });
+
+  // no error 
+  next();
+}
 
 
 
