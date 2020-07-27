4f7c515faa94f74fba885dcdbbd350bb8d6e6dfb
omid saghatchian <omidsaghatchian@gmail.com>
2020-07-27 16:45:14 +0430

    Create for upload avatar image

---
+++ tools/uploadArticleAvatarImage.js
@@ -0,0 +1,42 @@
+const multer = require("multer");
+const path = require("path");
+
+// Set storage engine
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './public/uploads/articleAvatar')
+  },
+  filename: function(req, file, cb) {
+    cb(null, 'articleAvatar' + '-' + file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+  }
+});
+
+
+
+
+// Init upload 
+module.exports.uploadImage = multer({
+  storage: storage, 
+  limits: {
+    fileSize: 10000000
+  },
+
+  fileFilter: function(req, file, cb){
+
+    // allowed ext
+    const fileTypes = /jpeg|jpg|png|gif/ ; 
+    // check the extension 
+    const extname = fileTypes.test(path.extname(file.originalname).toLocaleLowerCase());
+    // check meme
+    const mimeType = fileTypes.test(file.mimetype);
+
+    if(mimeType && extname){
+      return cb (null, true);
+    } else {
+      cb ('Error: Images Only!')
+    }
+  }
+})
+
+
+
