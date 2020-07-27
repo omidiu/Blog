cd1ef452d7842caaee531007fc4b040caa98b99f
omid saghatchian <omidsaghatchian@gmail.com>
2020-07-27 08:49:07 +0430

    add "articleRouter"

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ try {
 const indexRouter = require('./routes/index');
 const userRouter = require('./routes/user');
 const commentRouter = require('./routes/comment');
-
+const articleRouter = require('./routes/article');
 
 const { log } = require('console');
 
@@ -36,13 +36,14 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', userRouter);
 app.use('/comments', commentRouter);
+app.use('/articles', articleRouter);
 
 
 // catch 404 and forward to error handler
