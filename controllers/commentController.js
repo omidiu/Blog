58749072b85fd4213d8594f28fc572357db71dd7
omid saghatchian <omidsaghatchian@gmail.com>
2020-08-05 16:50:58 +0430

    Implement "addCommentToArticle" and remove "getComments"

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,15 +1,6 @@
-const Comment = require('comment');
-
-
-
-/*********************************************************************************
-* Send all comments of a specific article (GET)
-**********************************************************************************/
-exports.getComments = (req, res) => {
-  res.send(`Not implemented yet: send all comments of specific article ${req.params.articleId}`);
-};
-
-
+const Comment = require('../models/comment');
+const Article = require('../models/article');
+const mongoose = require('mongoose');
 
 
 
@@ -17,6 +8,67 @@ exports.getComments = (req, res) => {
 /*********************************************************************************
 * Add a comment to a specific article (POST)
 **********************************************************************************/
-exports.addComment = (req, res) => {
-  res.send(`Not implemented yet: add a comment to a specific article ${req.params.articleId}`)
+exports.addCommentToArticle = async (req, res) => {
+
+  
+
+
+  try {
+
+    // Get article id.
+    let articleId = req.params.articleId;
+
+    // Get author id in token.
+    let authorId = req.user._id;
+
+    // Get comment content
+    let commentContent = req.body.content;
+
+    // Check article id is valid or not
+    if (!mongoose.Types.ObjectId.isValid(articleId)) res.status(400).send("Bad Request");
+
+
+    
+    // find article 
+    let article = await Article.findById({_id: articleId});
+    if (!article) res.status(400).json({
+      message: "Article not found"
+    }) // if article is empty
+
+
+    // check comment is empty or not
+    if (!commentContent) 
+      return res.status(400).json({
+        message: "Your comment can't be empty"
+      }); 
+
+    
+
+    const comment = new Comment({
+      content: commentContent,
+      author: authorId,
+      article: articleId
+    });
+
+
+
+    const savedComment = await comment.save();
+
+    res.status("200").json({
+      message: "Your comment added successfully, refresh the page to see your comment"
+    }); // should change later maybe
+
+
+
+  } catch(err) {
+    console.log(err);
+    res.status(500).send("Error Occurred !!");
+  }
+  
+  
 };  
+
+
+
+
+
